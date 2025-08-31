d468f278852adf65bde14d827b56abceb5b1e863
John Dennehy <johndennehy101@gmail.com>
2025-08-31 16:41:45 +0100

    chore: initial frontend configuration

---
+++ frontend/src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import { JSX } from 'react'
+
+export function Home(): JSX.Element {
+  return (
+    <div className="text-center">
+      <h1 className="text-4xl font-bold text-gray-900 dark:text-white mb-6">
+        Note Taking App
+      </h1>
+    </div>
+  )
+}
